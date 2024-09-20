1b27ac2474dc6e5dc074708a545ba234d05336c7
dchorazkiewicz <damian.ch.datascience@gmail.com>
2024-09-20 16:32:27 +0200

    structure change

--- setup.sh
+++ setup.sh
@@ -6,109 +6,97 @@ mkdir -p Notebooks_EN
 
 # Make subdirectories for Polish version
 cd Notebooks_PL
-mkdir -p 01_Algebra_Liniowa/PY
-mkdir -p 01_Algebra_Liniowa/Notebooks
-mkdir -p 02_Geometria_Analityczna/PY
-mkdir -p 02_Geometria_Analityczna/Notebooks
-mkdir -p 03_Analiza_Matematyczna/PY
-mkdir -p 03_Analiza_Matematyczna/Notebooks
+mkdir -p 01_Algebra_Liniowa
+mkdir -p 02_Geometria_Analityczna
+mkdir -p 03_Analiza_Matematyczna
 
 ############################################
 # Algebra Liniowa
-cd 01_Algebra_Liniowa/PY
+cd 01_Algebra_Liniowa
 
-touch 01_a_Macierze_definicje.py
-touch 01_b_Macierze_dzialania.py
-touch 02___Uklady_Rownan_Liniowych.py
-touch 03_a_Wektory_definicje.py
-touch 04_b_Wektory_dzialania.py
+mkdir -p  01_Macierze
+mkdir -p  02_Uklady_Rownan_Liniowych
+mkdir -p  03_Wektory_definicje
+mkdir -p  04_Wektory_dzialania
 
 ############################################
 # Geometria Analityczna
-cd ../../02_Geometria_Analityczna/PY
+cd ../02_Geometria_Analityczna
 
-touch 05_Proste.py
-touch 06_Krzywe_Stozkowe.py
-touch 07_Plaszczyzny_W_Przestrzeni.py
-touch 08_Powierzchnie_Drugiego_Rzedu.py
-touch 09_Podstawy_Rachunku_Tensorowego.py
+mkdir -p  05_Proste
+mkdir -p  06_Krzywe_Stozkowe
+mkdir -p  07_Plaszczyzny_W_Przestrzeni
+mkdir -p  08_Powierzchnie_Drugiego_Rzedu
+mkdir -p  09_Podstawy_Rachunku_Tensorowego
 
 ############################################
 # Analiza Matematyczna
-cd ../../03_Analiza_Matematyczna/PY
+cd ../03_Analiza_Matematyczna
 # Teoria mnogości
-touch 10_a_Ciagi_definicje.py
-touch 10_b_Ciagi_granica.py
-touch 11_a_Funkcje_definicje.py
-touch 11_b_Funkcje_granica_i_ciaglosc.py
-touch 12_Pochodna_Funkcji.py
-touch 13_Rozniczka_i_Zastosowania.py
-touch 14_Zastosowania_Rachunku_Rozniczkowego.py
-touch 15_Calka_Nieoznaczona.py
-touch 16_Metody_calkowania_I.py
-touch 17_Metody_calkowania_II.py
-touch 18_Calka_Oznaczona.py
-touch 19_Calki_Niewlasciwe.py
-touch 20_Zastosowania_Calek_Oznaczonych.py
-touch 21_Szeregi_Liczbowe.py
-touch 22_Zbieznosc_Szeregow.py
-touch 23_Szeregi_Funkcyjne.py
-touch 24_Szeregi_Taylora_i_Maclaurina.py
-touch 25_Szeregi_Fouriera.py
+mkdir -p  10_Ciagi
+mkdir -p  11_Funkcje
+mkdir -p  12_Pochodna_Funkcji
+mkdir -p  13_Rozniczka_i_Zastosowania
+mkdir -p  14_Zastosowania_Rachunku_Rozniczkowego
+mkdir -p  15_Calka_Nieoznaczona
+mkdir -p  16_Metody_calkowania_I
+mkdir -p  17_Metody_calkowania_II
+mkdir -p  18_Calka_Oznaczona
+mkdir -p  19_Calki_Niewlasciwe
+mkdir -p  20_Zastosowania_Calek_Oznaczonych
+mkdir -p  21_Szeregi_Liczbowe
+mkdir -p  22_Zbieznosc_Szeregow
+mkdir -p  23_Szeregi_Funkcyjne
+mkdir -p  24_Szeregi_Taylora_i_Maclaurina
+mkdir -p  25_Szeregi_Fouriera
 
 ############################################
 # English Version
-cd ../../../Notebooks_EN
+cd ../../Notebooks_EN
 
-mkdir -p 01_Linear_Algebra/PY
-mkdir -p 01_Linear_Algebra/Notebooks
-mkdir -p 02_Analytical_Geometry/PY
-mkdir -p 02_Analytical_Geometry/Notebooks
-mkdir -p 03_Mathematical_Analysis/PY
-mkdir -p 03_Mathematical_Analysis/Notebooks
+mkdir -p 01_Linear_Algebra
+mkdir -p 02_Analytical_Geometry
+mkdir -p 03_Mathematical_Analysis
 
 ############################################
 # Linear Algebra
-cd 01_Linear_Algebra/PY
+cd 01_Linear_Algebra
 
-touch 01_a_Matrices_definitions.py
-touch 01_b_Matrices_operations.py
-touch 02_Systems_of_Linear_Equations.py
-touch 03_a_Vectors_definitions.py
-touch 04_b_Vectors_operations.py
+mkdir -p  01_Matrices
+mkdir -p  02_Systems_of_Linear_Equations
+mkdir -p  03_Vectors_definitions
+mkdir -p  04_Vectors_operations
 
 ############################################
 # Analytical Geometry
-cd ../../02_Analytical_Geometry/PY
+cd ../02_Analytical_Geometry
 
-touch 05_Lines.py
-touch 06_Conic_Curves.py
-touch 07_Planes_in_Space.py
-touch 08_Second_Order_Surfaces.py
-touch 09_Basics_of_Tensor_Calculus.py
+mkdir -p  05_Lines
+mkdir -p  06_Conic_Curves
+mkdir -p  07_Planes_in_Space
+mkdir -p  08_Second_Order_Surfaces
+mkdir -p  09_Basics_of_Tensor_Calculus
 
 ############################################
 # Mathematical Analysis
-cd ../../03_Mathematical_Analysis/PY
+cd ../03_Mathematical_Analysis
 
-touch 10_a_Sequences_definitions.py
-touch 10_b_Sequences_limit.py
-touch 11_a_Functions_definitions.py
-touch 11_b_Functions_limit_and_continuity.py
-touch 12_Function_Derivative.py
-touch 13_Differential_and_Applications.py
-touch 14_Applications_of_Differential_Calculus.py
-touch 15_Indefinite_Integral.py
-touch 16_Integration_Methods_I.py
-touch 17_Integration_Methods_II.py
-touch 18_Definite_Integral.py
-touch 19_Improper_Integrals.py
-touch 20_Applications_of_Defined_Integrals.py
-touch 21_Number_Series.py
-touch 22_Series_Convergence.py
-touch 23_Functional_Series.py
-touch 24_Taylor_and_Maclaurin_Series.py
-touch 25_Fourier_Series.py
+mkdir -p  10_Sequences
+mkdir -p  11_Functions
+mkdir -p  12_Function_Derivative
+mkdir -p  13_Differential_and_Applications
+mkdir -p  14_Applications_of_Differential_Calculus
+mkdir -p  15_Indefinite_Integral
+mkdir -p  16_Integration_Methods_I
+mkdir -p  17_Integration_Methods_II
+mkdir -p  18_Definite_Integral
+mkdir -p  19_Improper_Integrals
+mkdir -p  20_Applications_of_Definite_Integrals
+mkdir -p  21_Number_Series
+mkdir -p  22_Series_Convergence
+mkdir -p  23_Functional_Series
+mkdir -p  24_Taylor_and_Maclaurin_Series
+mkdir -p  25_Fourier_Series
 
 # List the directories and files
 ls -la
